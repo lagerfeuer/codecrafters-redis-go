203d0c770963ddb396666b85e0b42f1f46b3f628
Lukas Deutz <github@deutz.io>
2024-04-16 21:02:40 -0400

    Respond to multiple PINGs

--- app/server.go
+++ app/server.go
@@ -3,32 +3,36 @@ package main
 import (
 	"fmt"
 	"net"
-	"os"
 )
 
 func handle(conn net.Conn) {
-	var buffer []byte
+	fmt.Println("$$$ handle $$$")
 
-	_, err := conn.Read(buffer)
-	if err != nil {
-		fmt.Println("Could not read from connection: ", err.Error())
+	for {
+		buffer := make([]byte, 128)
+
+		_, err := conn.Read(buffer)
+		if err != nil {
+			fmt.Println("Could not read from connection: ", err.Error())
+			return
+		}
+
+		conn.Write([]byte("+PONG\r\n"))
 	}
-
-	conn.Write([]byte("+PONG\r\n"))
 }
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379: ", err.Error())
-		os.Exit(1)
+		panic(err)
 	}
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			panic(err)
+		}
 
-	handle(conn)
+		handle(conn)
+	}
 }
