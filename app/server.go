227ff94f5f51b43fc0ff60619d8513a603df7fee
Lukas Deutz <github@deutz.io>
2024-04-16 20:53:03 -0400

    Respond to PING

--- app/server.go
+++ app/server.go
@@ -6,16 +6,29 @@ import (
 	"os"
 )
 
-func main() {
+func handle(conn net.Conn) {
+	var buffer []byte
 
+	_, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Could not read from connection: ", err.Error())
+	}
+
+	conn.Write([]byte("+PONG\r\n"))
+}
+
+func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
+		fmt.Println("Failed to bind to port 6379: ", err.Error())
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	handle(conn)
 }
